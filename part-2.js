ac31b1518b8a54920cf582cedd0d875809627133
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 11:06:44 +0100

    Finish part 2

--- part-2.js
+++ part-2.js
@@ -1,1 +1,20 @@
-console.log('your code here')
+// Part 2
+
+const fs = require('fs');
+
+const fileContents = fs.readFileSync('shopping-basket.json', 'utf-8');
+const data = JSON.parse(fileContents);
+
+console.log(data);
+
+for (let item of data.basket) {
+  console.log(`${item.name}: $${item.price * item.quantity}`)
+}
+
+const index = data.basket.findIndex((item) => {
+  return item.name == 'Candles';
+});
+
+data.basket[index].quantity = 10;
+const jsonString = JSON.stringify(data, null, 2);
+fs.writeFileSync('new-basket.json', jsonString, 'utf-8');
