38d11d664bf0cf327395a2d9d603f23c257ad50d
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 10:47:14 +0100

    Finish challenge part 1

--- part-1.js
+++ part-1.js
@@ -3,8 +3,17 @@
 const fs = require('fs');
 
 const fileContents = fs.readFileSync('names.txt', 'utf-8');
-const reversedContents = fileContents.split("\n").reverse().join("\n");
+const arrayOfNames = fileContents.split("\n");
+// slice for not mutate original array
+const reversedContents = arrayOfNames.slice().reverse().join("\n");
 
 console.log(reversedContents);
 
 fs.writeFileSync('names-reversed.txt', reversedContents, 'utf-8');
+
+// Part 1 Challenge
+
+arrayOfNames.forEach((name, i) => {
+  let reversedName = name.split("").reverse().join("");
+  fs.writeFileSync(`${name}-${i}.txt`, reversedName, 'utf-8');
+});
