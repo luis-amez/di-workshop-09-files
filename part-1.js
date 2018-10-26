e0ebd319a5b2619a05a6ad9d1478a8888366d39a
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 10:32:07 +0100

    Finish part 1

--- part-1.js
+++ part-1.js
@@ -1,1 +1,10 @@
-console.log('your code here')
+// Part 1
+
+const fs = require('fs');
+
+const fileContents = fs.readFileSync('names.txt', 'utf-8');
+const reversedContents = fileContents.split("\n").reverse().join("\n");
+
+console.log(reversedContents);
+
+fs.writeFileSync('names-reversed.txt', reversedContents, 'utf-8');
